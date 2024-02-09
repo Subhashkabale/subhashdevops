c946d7c84c152c7733e2d7f23720e8c0d7e2836f
subhdevopsuser1 <subhashkabale8668@gmail.com>
2024-02-09 08:46:49 +0530

    commit1

---
+++ file1.java
@@ -0,0 +1,41 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+
+git init
+
+git config --global user.name "subhdevopsuser1"
+
+git config --global user.email subhashkabale8668@gmail.com
+
+git config --global --list
+
+git status
+
+git add file1.java
+
+git add .
+
+git commit -m "commit"
+
+
+git commit -m "commit1"
+
+git log
+
+git log --oneline
+
+
+git restore --staged file9.java
+git commit -m "commit2"
+
+
+git revert HEAD --no-edit
+
+
+07cf770 
+
+git reset 07cf770
